e60a57b1d9d3ab7586b651c1af51f2b20e11341b
rolandm99 <manfulmweze99@gmail.com>
2025-02-11 10:40:09 +0200

    chore: add render services configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+  });
+
   await app.listen(3000);
   console.log('App running on port 3000');
 }
