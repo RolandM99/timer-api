e6536ca3ea250b4743edfc680d982f37598ce11b
rolandm99 <manfulmweze99@gmail.com>
2025-02-11 11:07:28 +0200

    chore: add production environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { TimerModule } from './timer/timer.module';
 import { Timer } from './timer/entities/timer.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'timer_db',
+      ...(process.env.NODE_ENV === 'production'
+        ? {
+            url: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'postgres',
+            database: 'timer_db',
+          }),
       entities: [Timer],
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
     TimerModule,
   ],
