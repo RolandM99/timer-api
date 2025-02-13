0e2835a57372e25348b08ec505f74b122939538c
rolandm99 <manfulmweze99@gmail.com>
2025-02-13 20:21:12 +0200

    fix: update endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: '*',
+    origin: true,
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
   });
 
   await app.listen(3000);
